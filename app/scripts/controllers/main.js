b91da723145ad0757cc0a3bd817ecd86a691f04e
Patrick Sullivan <sullivanpt@acm.org>
2014-01-18 15:55:45 -0800

    after yo angular, using generator-angular@0.7.1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('phonegapAngularClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
