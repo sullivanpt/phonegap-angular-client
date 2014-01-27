fc66d06be752924c7c770f17e98c9f184ce939fc
Patrick Sullivan <sullivanpt@acm.org>
2014-01-27 01:13:56 -0800

    Mock out resource owner password authentication page

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -5,7 +5,9 @@ angular.module('phonegapAngularClientApp')
     return {
       templateUrl: 'views/navbar.html',
       restrict: 'E',
-      controller: ['$scope', '$location', 'cordovaReady', function ($scope, $location) {
+      controller: ['$scope', '$location', 'cordovaReady', function ($scope, $location, cordovaReady) {
+        cordovaReady(); // bootstrap the event listener. TODO: change this to angular.run()
+
         $scope.isActive = function(route) {
           return route === $location.path();
         };
