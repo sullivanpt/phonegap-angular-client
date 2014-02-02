94d4a5cf3acbff849c6b0cc86105426f39711467
Patrick Sullivan <sullivanpt@acm.org>
2014-02-02 13:31:37 -0800

    fix deviceready immediately  bug

--- app/scripts/services/cordova-ready.js
+++ app/scripts/services/cordova-ready.js
@@ -11,12 +11,16 @@
 'use strict';
 
 angular.module('phonegapAngularClientApp')
-  .factory('cordovaReady', function ($rootScope) {
+  .factory('cordovaReady', function ($rootScope, $timeout) {
 
     var status = false;
     document.addEventListener('deviceready', function () {
       console.log('Cordova deviceready received.');
-      $rootScope.$apply(function () {
+      // deviceready is sometimes a direct callback if phonegap is already loaded (no $apply needed),
+      // and is a DOM event if loaded in the future ($apply is needed).
+      // Apparently the safe way to make sure it's always wrapped correctly is to use $timeout.
+      // http://stackoverflow.com/a/18996042
+      $timeout(function () {
         status = true;
         $rootScope.cordovaIsReady = true; // Provide a simplified global flag
       });
