3bb8ebf0116ad2208cdfa08b5dc6b01a585b49cf
Patrick Sullivan <sullivanpt@acm.org>
2014-03-02 20:28:36 -0800

    show app version in footer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('phonegapAngularClientApp')
-  .controller('MainCtrl', function ($scope, cordovaReady, $window) {
+  .controller('MainCtrl', function ($scope, cordovaReady, $window, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('version.json').success(function(version) {
+      $scope.version = version;
+    });
 
     $scope.$watch(cordovaReady, function (newValue) {
       if (newValue) {
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,10 +6,14 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('phonegapAngularClientApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('version.json')
+      .respond({revision:'abc1234',date:'2014-03-02T19:36:34'});
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
@@ -18,5 +22,10 @@ describe('Controller: MainCtrl', function () {
 
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
+  });
+
+  it('should attach the version to the scope', function () {
+    $httpBackend.flush();
+    expect(scope.version.revision).toBe('abc1234');
   });
 });
